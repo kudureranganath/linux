6865a0868d76321612b490eedc9c990fa8b02b67
K Prateek Nayak <kprateek.nayak@amd.com>
2025-05-27 03:41:38 +0000

    Revert "sched/fair: Add lag based placement"
    This reverts commit 86bfbb7ce4f67a88df2639198169b685668e7349.
    
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -554,9 +554,8 @@ struct sched_entity {
 
 	u64				exec_start;
 	u64				sum_exec_runtime;
-	u64				prev_sum_exec_runtime;
 	u64				vruntime;
-	s64				vlag;
+	u64				prev_sum_exec_runtime;
 
 	u64				nr_migrations;
 
